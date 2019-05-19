a839bb35e99419376ce170dc050b671c4af03c36
Pankaj Vaghela <vpankaj1998dev@gmail.com>
2019-05-19 12:11:04 +0530

    Name change from crystlPHP to CrystalPHP
    Took 36 seconds

--- core/engine/index.php
+++ core/engine/index.php
@@ -1,9 +1,8 @@
 <?php
-
 /*******************************************************************************
  *
- *   This file is part of CrystlPHP Framework.
- *   Copyright (c) 2019. CrystlPHP Collective.
+ *   This file is part of CrystalPHP Framework.
+ *   Copyright (c) 2019. Crystal Collective.
  *
  *   For the full copyright and license information, please view the LICENSE
  *   file that was distributed with this source code.
--- core/router/index.php
+++ core/router/index.php
@@ -1,8 +1,9 @@
 <?php
+
 /*******************************************************************************
  *
- *   This file is part of CrystlPHP Framework.
- *   Copyright (c) 2019. CrystlPHP Collective.
+ *   This file is part of CrystalPHP Framework.
+ *   Copyright (c) 2019. Crystal Collective.
  *
  *   For the full copyright and license information, please view the LICENSE
  *   file that was distributed with this source code.
